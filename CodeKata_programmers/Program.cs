289b390fdcfc5745c24dce087d5f12118b3379cf
hoihoi <hiimhoihoi@gmail.com>
2023-08-24 18:39:03 +0900

    초기 커밋.

---
+++ CodeKata_programmers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeKata_programmers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
